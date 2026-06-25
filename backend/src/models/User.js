3a2d149e60f2094e40d845b313d6acea215ec460
xcuongcs5 <xcuongcs50@gmail.com>
2026-06-25 20:27:22 +0700

    auth and db sync

---
+++ backend/src/models/User.js
@@ -0,0 +1,27 @@
+import { request } from "express";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    profileImage: {
+        type:String,
+        default:""
+    },
+    clerlId:{
+        type:String,
+        required:true,
+        unique:true
+    }
+}, {timestamps:true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
