8c38a032ba384e7ff46c8f0732039545a53c7b27
xcuongcs5 <xcuongcs50@gmail.com>
2026-06-25 22:29:51 +0700

    auth & stream sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import {Inngest} from 'inngest';
 import { connectDB } from './db.js';
 import User from '../models/User.js';
+import { deleteStreamUser, upsertStreamUser } from './stream.js';
 
 export const inngest = new Inngest({id: "interview-smart"});
 
@@ -20,7 +21,13 @@ const syncUser = inngest.createFunction(
             profileImage: image_url
         }
 
-        await User.create(newUser)
+        await User.create(newUser);
+
+        await upsertStreamUser({
+            id: newUser.clerkId.toString(),
+            name: newUser.name,
+            image: newUser.profileImage
+        });
     }
 );
 
@@ -33,6 +40,8 @@ const deleteUserFromDB = inngest.createFunction(
         const {id} = event.data;
 
         await User.deleteOne({ clerkId: id });
+
+        await deleteStreamUser(id.toString())
     }
 );
 
